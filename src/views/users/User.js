ee1c92762e0cb4a065706cccb4605030554b060d
IceChaijaroen <ice_ice.04@hotmail.com>
2021-10-11 19:07:16 +0700

    ADMIN delete/edit(90%)

--- src/views/users/User.js
+++ src/views/users/User.js
@@ -4,20 +4,80 @@ import CIcon from '@coreui/icons-react'
 import axios from 'axios'
 
 
-const User = ({match}) => {
+const User = ({ match }) => {
   const [usersData, setUserinfo] = useState([]);
+  const [iduser, setIduser] = useState([]);
+  const [username, setUsername] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [name, setName] = useState();
+  const [lastname, setLastname] = useState();
+  const [img, setImg] = useState();
+  const [submit, setSubmit] = useState(false)
+
   useEffect(() => {
-    axios.get('http://35.187.253.40/admin/user.php')
+    axios.get('http://35.187.253.40/admin/showedituser.php', {
+      params: {
+        iduser: match.params.id
+      }
+    })
       .then(res => {
-        setUserinfo(res.data);
+        setUserinfo(res.data.all);
+        setUsername(res.data.username);
+        setEmail(res.data.email);
+        setPassword(res.data.password);
+        setName(res.data.name);
+        setLastname(res.data.lastname);
+        setImg(res.data.img);
       })
       .catch(err => {
         alert(err)
       })
-  }, [usersData])
-  
-  const user = usersData.find( user => user.iduser === match.params.id)
-  const userDetails = user ? Object.entries(user) : 
+  }, [])
+
+
+  function Submit(e) {
+    e.preventDefault();
+    axios.post('http://35.187.253.40/admin/update.php', {
+      iduser: parseInt(match.params.id),
+      username: username,
+      email: email,
+      password: password,
+      name: name,
+      lastname: lastname,
+      img: img
+    })
+      .then(res => {
+        alert(res.data);
+      })
+  }
+
+
+  const uploadImage = async (e) => {
+    const file = e.target.files[0];
+    const base64 = await convertBase64(file);
+    setImg(base64);
+  };
+
+  const convertBase64 = (file) => {
+    return new Promise((resolve, reject) => {
+      const fileReader = new FileReader();
+      fileReader.readAsDataURL(file);
+
+      fileReader.onload = () => {
+        resolve(fileReader.result);
+      };
+
+      fileReader.onerror = (error) => {
+        reject(error);
+      };
+    });
+  };
+
+
+  console.log(img)
+  const user = usersData.find(user => user.iduser === match.params.id)
+  const userDetails = user ? Object.entries(user) :
     [['id', (<span><CIcon className="text-muted" name="cui-icon-ban" /> Not found</span>)]]
 
   return (
@@ -28,20 +88,59 @@ const User = ({match}) => {
             User id: {match.params.id}
           </CCardHeader>
           <CCardBody>
+            <form onSubmit={(e) => Submit(e)}>
               <table className="table table-striped table-hover">
+
                 <tbody>
                   {
                     userDetails.map(([key, value], index) => {
                       return (
                         <tr key={index}>
                           <td>{`${key}:`}</td>
-                          <td><strong>{value}</strong></td>
+                          {(() => {
+                            if (key == 'img') {
+                              return (
+                                <>
+                                  <td>
+                                    <img src={img} />
+                                    <input type="file" onChange={(e) => uploadImage(e)} />
+                                  </td>
+                                </>
+                              )
+                            } else if (key == 'username') {
+                              return (
+                                <td><input type="text" value={username} onChange={e => setUsername(e.target.value)} /></td>
+                              )
+                            } else if (key == 'email') {
+                              return (
+                                <td><input type="text" value={email} onChange={e => setEmail(e.target.value)} /></td>
+                              )
+                            } else if (key == 'password') {
+                              return (
+                                <td><input type="text" value={password} onChange={e => setPassword(e.target.value)} /></td>
+                              )
+                            } else if (key == 'name') {
+                              return (
+                                <td><input type="text" value={name} onChange={e => setName(e.target.value)} /></td>
+                              )
+                            } else if (key == 'lastname') {
+                              return (
+                                <td><input type="text" value={lastname} onChange={e => setLastname(e.target.value)} /></td>
+                              )
+                            } else {
+                              return (
+                                <td><input type="text" value={value} disabled={true} /></td>
+                              )
+                            }
+                          })()}
                         </tr>
                       )
                     })
                   }
                 </tbody>
+                <button > ยืนยัน </button>
               </table>
+            </form>
           </CCardBody>
         </CCard>
       </CCol>
--- src/views/users/Users.js
+++ src/views/users/Users.js
@@ -8,97 +8,149 @@ import {
   CCol,
   CDataTable,
   CRow,
-  CPagination
+  CPagination,
+  CButton,
+  CCollapse
 } from '@coreui/react'
 
-import usersData from './UsersData'
+
 import axios from 'axios'
 
 
 
-
-
-const getBadge = status => {
-  switch (status) {
-    case 'Active': return 'success'
-    case 'Inactive': return 'secondary'
-    case 'Pending': return 'warning'
-    case 'Banned': return 'danger'
-    default: return 'primary'
-  }
-}
-
 const Users = () => {
+  const [details, setDetails] = useState([])
+  const [userinfo, setUserinfo] = useState([])
+  const [submit, setSubmit] = useState(false)
+  const [iduser, setUserid] = useState()
   const history = useHistory()
   const queryPage = useLocation().search.match(/page=([0-9]+)/, '')
   const currentPage = Number(queryPage && queryPage[1] ? queryPage[1] : 1)
   const [page, setPage] = useState(currentPage)
-  const [userinfo, setUserinfo] = useState([]);
 
   const pageChange = newPage => {
     currentPage !== newPage && history.push(`/users?page=${newPage}`)
   }
 
   useEffect(() => {
-    currentPage !== page && setPage(currentPage)
-  }, [currentPage, page])
-
-  useEffect(() => {
-      axios.get('http://35.187.253.40/admin/user.php')
+    axios.get('http://35.187.253.40/admin/user.php')
       .then(res => {
-         setUserinfo(res.data);
+        setUserinfo(res.data);
       })
       .catch(err => {
         alert(err)
       })
-  },[userinfo])
+  }, [userinfo])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get('http://35.187.253.40/admin/delete.php', {
+          params: {
+            iduser: iduser
+          }
+        })
+        alert(res.data);
+      } catch (err) {
+        alert(err);
+      }
+    }
+    if (submit) fetchData();
+  }, [submit])
+
+  const toggleDetails = (index) => {
+    const position = details.indexOf(index)
+    let newDetails = details.slice()
+    if (position !== -1) {
+      newDetails.splice(position, 1)
+    } else {
+      newDetails = [...details, index]
+    }
+    setDetails(newDetails)
+  }
 
 
+  const fields = [
+    { key: 'name', _style: { width: '40%' } },
+    'registered',
+    { key: 'email', _style: { width: '20%' } },
+    { key: 'status', _style: { width: '20%' } },
+    {
+      key: 'show_details',
+      label: '',
+      _style: { width: '1%' },
+      sorter: false,
+      filter: false
+    }
+  ]
 
+  const getBadge = (status) => {
+    switch (status) {
+      case 'Active': return 'success'
+      case 'Inactive': return 'secondary'
+      case 'Pending': return 'warning'
+      case 'Banned': return 'danger'
+      default: return 'primary'
+    }
+  }
 
   return (
-    <CRow>
-      <CCol xl={12}>
-        <CCard>
-          <CCardHeader>
-            Users
-            <small className="text-muted"> All</small>
-          </CCardHeader>
-          <CCardBody>
-            <CDataTable
-              items={userinfo}
-              fields={[
-                { key: 'name', _classes: 'font-weight-bold' },
-                'email'
-              ]}
-              hover
-              striped
-              itemsPerPage={5}
-              activePage={page}
-              clickableRows
-              onRowClick={(item) => history.push(`/users/${item.iduser}`)}
-              scopedSlots={{
-                'status':
-                  (item) => (
-                    <td>
-                      <CBadge color={getBadge(item.status)}>
-                        {item.status}
-                      </CBadge>
-                    </td>
-                  )
-              }}
-            />
-            <CPagination
-              activePage={page}
-              onActivePageChange={pageChange}
-              pages={5}
-              doubleArrows={false}
-              align="center"
-            />
-          </CCardBody>
-        </CCard>
-      </CCol>
-    </CRow>
+    <CDataTable
+      items={userinfo}
+      fields={fields}
+      columnFilter
+      tableFilter
+      footer
+      itemsPerPageSelect
+      itemsPerPage={5}
+      hover
+      sorter
+      pagination
+      scopedSlots={{
+        'status':
+          (item) => (
+            <td>
+              <CBadge color={getBadge(item.status)}>
+                {item.status}
+              </CBadge>
+            </td>
+          ),
+        'show_details':
+          (item, index) => {
+            return (
+              <td className="py-2">
+                <CButton
+                  color="primary"
+                  variant="outline"
+                  shape="square"
+                  size="sm"
+                  onClick={() => { toggleDetails(index) }}
+                >
+                  {details.includes(index) ? 'Hide' : 'Show'}
+                </CButton>
+              </td>
+            )
+          },
+        'details':
+          (item, index) => {
+            return (
+              <CCollapse show={details.includes(index)}>
+                <CCardBody>
+                  <h4>
+                    {item.name}
+                  </h4>
+                  <CButton size="sm" color="info" onClick={() => history.push(`/users/${item.iduser}`)}>
+                    User Settings
+                  </CButton>
+                  <CButton size="sm" color="danger" className="ml-1" onClick={() => { if (window.confirm('ยืนยันการลบข้อมูล')) setSubmit(true); setUserid(item.iduser) }}>
+                    Delete
+                  </CButton>
+                </CCardBody>
+              </CCollapse>
+            )
+          }
+      }}
+    />
   )
 }
 
