ab7bbf5df62e98e7d2c80cb4c122448329c29521
IceChaijaroen <ice_ice.04@hotmail.com>
2021-10-26 22:30:16 +0700

    Dashboard(100%) admin done git add .

--- src/views/charts/MainChartExample.js
+++ src/views/charts/MainChartExample.js
@@ -1,99 +1,105 @@
-import React from 'react'
-import { CChartLine } from '@coreui/react-chartjs'
+import React, { useEffect, useState } from 'react'
+import { CChartLine, CChartBar } from '@coreui/react-chartjs'
 import { getStyle, hexToRgba } from '@coreui/utils'
+import axios from 'axios'
 
 const brandSuccess = getStyle('success') || '#4dbd74'
 const brandInfo = getStyle('info') || '#20a8d8'
 const brandDanger = getStyle('danger') || '#f86c6b'
 
+
 const MainChartExample = attributes => {
-  const random = (min, max)=>{
+  const [chart, setChart] = useState([]);
+  const random = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min)
   }
 
-  const defaultDatasets = (()=>{
+  const DataDashboard = async () => {
+    try {
+      const chart = await axios.get('http://35.187.253.40/admin/dashboard/chartuser.php');
+      setChart(chart.data);
+    } catch (err) {
+      alert(err)
+    }
+  }
+
+  useEffect(() => {
+    DataDashboard();
+  }, []);
+
+
+  const month = ['', 'มกราคม', 'กุมภาพันธุ์', 'มีนาคม', 'เมษายน', 'พฤษภาคม', 'มิถุนายน', 'กรกฎาคม', 'สิงหาคม', 'กันยายน', 'ตุลาคม', 'พฤศจิกายน', 'ธันวาคม'];
+  const findindex = chart.map(item => month[(item.month)])
+
+  console.log(findindex)
+  const defaultDatasets = (() => {
     let elements = 27
-    const data1 = []
+    const data1 = chart.map(item => (item.user))
     const data2 = []
     const data3 = []
     for (let i = 0; i <= elements; i++) {
-      data1.push(random(50, 200))
+      chart.map(item => (item.user))
       data2.push(random(80, 100))
       data3.push(65)
     }
     return [
       {
-        label: 'My First dataset',
+        label: 'ผู้ใช้',
         backgroundColor: hexToRgba(brandInfo, 10),
         borderColor: brandInfo,
         pointHoverBackgroundColor: brandInfo,
         borderWidth: 2,
         data: data1
-      },
-      {
-        label: 'My Second dataset',
-        backgroundColor: 'transparent',
-        borderColor: brandSuccess,
-        pointHoverBackgroundColor: brandSuccess,
-        borderWidth: 2,
-        data: data2
-      },
-      {
-        label: 'My Third dataset',
-        backgroundColor: 'transparent',
-        borderColor: brandDanger,
-        pointHoverBackgroundColor: brandDanger,
-        borderWidth: 1,
-        borderDash: [8, 5],
-        data: data3
       }
     ]
   })()
 
-  const defaultOptions = (()=>{
+  const defaultOptions = (() => {
     return {
-        maintainAspectRatio: false,
-        legend: {
-          display: false
-        },
-        scales: {
-          xAxes: [{
-            gridLines: {
-              drawOnChartArea: false
-            }
-          }],
-          yAxes: [{
-            ticks: {
-              beginAtZero: true,
-              maxTicksLimit: 5,
-              stepSize: Math.ceil(250 / 5),
-              max: 250
-            },
-            gridLines: {
-              display: true
-            }
-          }]
-        },
-        elements: {
-          point: {
-            radius: 0,
-            hitRadius: 10,
-            hoverRadius: 4,
-            hoverBorderWidth: 3
+      maintainAspectRatio: false,
+      legend: {
+        display: false
+      },
+      scales: {
+        xAxes: [{
+          gridLines: {
+            drawOnChartArea: false
           }
+        }],
+        yAxes: [{
+          ticks: {
+            beginAtZero: true,
+            maxTicksLimit: 5,
+            stepSize: Math.ceil(20 / 9),
+            max: 20
+          },
+          gridLines: {
+            display: true
+          }
+        }]
+      },
+      elements: {
+        point: {
+          radius: 0,
+          hitRadius: 10,
+          hoverRadius: 4,
+          hoverBorderWidth: 3
         }
       }
     }
+  }
   )()
 
   // render
   return (
-    <CChartLine
-      {...attributes}
-      datasets={defaultDatasets}
-      options={defaultOptions}
-      labels={['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su']}
-    />
+    <>
+      <CChartBar
+        {...attributes}
+        datasets={defaultDatasets}
+        options={defaultOptions}
+        labels={findindex}
+      />
+    </>
   )
 }
 
--- src/views/widgets/WidgetsDropdown.js
+++ src/views/widgets/WidgetsDropdown.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {
   CWidgetDropdown,
   CRow,
@@ -11,132 +11,104 @@ import {
 import CIcon from '@coreui/icons-react'
 import ChartLineSimple from '../charts/ChartLineSimple'
 import ChartBarSimple from '../charts/ChartBarSimple'
+import axios from 'axios'
+import { useHistory, useLocation } from 'react-router-dom'
+
 
 const WidgetsDropdown = () => {
+  const [user, setUser] = useState();
+  const [alluser, setAlluser] = useState([]);
+  const [doguser, setUserdog] = useState();
+  const [doginfo, setDoginfo] = useState();
+  const [dogtrain, setDogtrain] = useState();
+
+  const DataDashboard = async () => {
+    try {
+      const doguser = await axios.get('http://35.187.253.40/admin/dashboard/doguser.php');
+      const doginfo = await axios.get('http://35.187.253.40/admin/dashboard/doginfo.php');
+      const user = await axios.get('http://35.187.253.40/admin/dashboard/user.php');
+      const dogtrain = await axios.get('http://35.187.253.40/admin/dashboard/dogtrain.php');
+
+      setUser(user.data.user);
+      setUserdog(doguser.data);
+      setDoginfo(doginfo.data);
+      setDogtrain(dogtrain.data);
+    } catch (err) {
+      alert(err)
+    }
+  }
+
+  useEffect(() => {
+    DataDashboard();
+  });
+
+  console.log(user)
+
   // render
   return (
     <CRow>
       <CCol sm="6" lg="3">
         <CWidgetDropdown
           color="gradient-primary"
-          header="9.823"
-          text="Members online"
+          header={user}
+          text="ผู้ใช้ทั้งหมด"
           footerSlot={
             <ChartLineSimple
-              pointed
               className="c-chart-wrapper mt-3 mx-3"
-              style={{height: '70px'}}
-              dataPoints={[65, 59, 84, 84, 51, 55, 40]}
-              pointHoverBackgroundColor="primary"
-              label="Members"
-              labels="months"
+              style={{ height: '20px' }}
+              dataPoints={[]}
             />
           }
         >
-          <CDropdown>
-            <CDropdownToggle color="transparent">
-              <CIcon name="cil-settings"/>
-            </CDropdownToggle>
-            <CDropdownMenu className="pt-0" placement="bottom-end">
-              <CDropdownItem>Action</CDropdownItem>
-              <CDropdownItem>Another action</CDropdownItem>
-              <CDropdownItem>Something else here...</CDropdownItem>
-              <CDropdownItem disabled>Disabled action</CDropdownItem>
-            </CDropdownMenu>
-          </CDropdown>
         </CWidgetDropdown>
+
       </CCol>
 
       <CCol sm="6" lg="3">
         <CWidgetDropdown
           color="gradient-info"
-          header="9.823"
-          text="Members online"
+          header={doguser}
+          text="สุนัขทั้งหมด"
           footerSlot={
             <ChartLineSimple
-              pointed
-              className="mt-3 mx-3"
-              style={{height: '70px'}}
-              dataPoints={[1, 18, 9, 17, 34, 22, 11]}
-              pointHoverBackgroundColor="info"
-              options={{ elements: { line: { tension: 0.00001 }}}}
-              label="Members"
-              labels="months"
+              className="c-chart-wrapper mt-3 mx-3"
+              style={{ height: '20px' }}
+              dataPoints={[]}
             />
           }
         >
-          <CDropdown>
-            <CDropdownToggle caret={false} color="transparent">
-              <CIcon name="cil-location-pin"/>
-            </CDropdownToggle>
-            <CDropdownMenu className="pt-0" placement="bottom-end">
-              <CDropdownItem>Action</CDropdownItem>
-              <CDropdownItem>Another action</CDropdownItem>
-              <CDropdownItem>Something else here...</CDropdownItem>
-              <CDropdownItem disabled>Disabled action</CDropdownItem>
-            </CDropdownMenu>
-          </CDropdown>
         </CWidgetDropdown>
       </CCol>
 
       <CCol sm="6" lg="3">
         <CWidgetDropdown
           color="gradient-warning"
-          header="9.823"
-          text="Members online"
+          header={doginfo}
+          text="สายพันธุ์สุนัขทั้งหมด"
           footerSlot={
             <ChartLineSimple
-              className="mt-3"
-              style={{height: '70px'}}
-              backgroundColor="rgba(255,255,255,.2)"
-              dataPoints={[78, 81, 80, 45, 34, 12, 40]}
-              options={{ elements: { line: { borderWidth: 2.5 }}}}
-              pointHoverBackgroundColor="warning"
-              label="Members"
-              labels="months"
+              className="c-chart-wrapper mt-3 mx-3"
+              style={{ height: '20px' }}
+              dataPoints={[]}
             />
           }
         >
-          <CDropdown>
-            <CDropdownToggle color="transparent">
-              <CIcon name="cil-settings"/>
-            </CDropdownToggle>
-            <CDropdownMenu className="pt-0" placement="bottom-end">
-              <CDropdownItem>Action</CDropdownItem>
-              <CDropdownItem>Another action</CDropdownItem>
-              <CDropdownItem>Something else here...</CDropdownItem>
-              <CDropdownItem disabled>Disabled action</CDropdownItem>
-            </CDropdownMenu>
-          </CDropdown>
         </CWidgetDropdown>
       </CCol>
 
       <CCol sm="6" lg="3">
         <CWidgetDropdown
           color="gradient-danger"
-          header="9.823"
-          text="Members online"
+          header={dogtrain}
+          text="ท่าฝึกทั้งหมด"
           footerSlot={
-            <ChartBarSimple
-              className="mt-3 mx-3"
-              style={{height: '70px'}}
-              backgroundColor="rgb(250, 152, 152)"
-              label="Members"
-              labels="months"
+            <ChartLineSimple
+              className="c-chart-wrapper mt-3 mx-3"
+              style={{ height: '20px' }}
+              dataPoints={[]}
             />
           }
         >
-          <CDropdown>
-            <CDropdownToggle caret className="text-white" color="transparent">
-              <CIcon name="cil-settings"/>
-            </CDropdownToggle>
-            <CDropdownMenu className="pt-0" placement="bottom-end">
-              <CDropdownItem>Action</CDropdownItem>
-              <CDropdownItem>Another action</CDropdownItem>
-              <CDropdownItem>Something else here...</CDropdownItem>
-              <CDropdownItem disabled>Disabled action</CDropdownItem>
-            </CDropdownMenu>
-          </CDropdown>
         </CWidgetDropdown>
       </CCol>
     </CRow>
